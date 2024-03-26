c21874db8fe35238ba26e0c3deb507e869df0dea
Manthan <bhatmanthan04@gmail.com>
2024-03-26 13:30:16 +0530

    added updateStatus route

--- src/routes/job.routes.js
+++ src/routes/job.routes.js
@@ -3,6 +3,7 @@ import {
     applyForJob,
     createJob
 } from "../controllers/job.controller.js";
+import { updateStatus } from "../controllers/company.controller.js";
 import { verifyJWTCo } from "../middlewares/auth.company.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -11,6 +12,7 @@ const router = Router()
 
 router.route("/apply/:jobId").post(verifyJWT,applyForJob)
 router.route("/create").post(verifyJWTCo,createJob)
+router.route("/:jobId/users/:userId/status").put(verifyJWTCo,updateStatus)
 
 // router.route("/getJob").get
 export default router
