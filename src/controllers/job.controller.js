7de428268d8ea4b671b98e13ce90b6861f3552ba
Manthan <bhatmanthan04@gmail.com>
2024-03-26 12:27:50 +0530

    fixed job controller

--- src/controllers/job.controller.js
+++ src/controllers/job.controller.js
@@ -4,41 +4,82 @@ import { User } from "../models/user.model.js";
 import { Job } from "../models/Job.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-const applyForJob = asyncHandler(async (req, res) => {
-    // try {
-        // Get the job ID from the request parameters
-        const { jobId } = req.params;
+// const applyForJob = asyncHandler(async (req, res) => {
+//     // try {
+//         // Get the job ID from the request parameters
+//         const { jobId } = req.params;
 
-        // Get the user ID from the request user object
-        const userId = req.user._id;
+//         // Get the user ID from the request user object
+//         const userId = req.user._id;
 
-        // Find the job by ID
-        const job = await Job.findById(jobId);
+//         // Find the job by ID
+//         const job = await Job.findById(jobId);
         
-        // Check if the job exists
-        if (!job) {
-            throw new ApiError(404, 'Job not found');
-        }
+//         // Check if the job exists
+//         if (!job) {
+//             throw new ApiError(404, 'Job not found');
+//         }
 
-        // Check if the user has already applied for this job
-        const existingApplicant = job.applicants.find(applicant => applicant.user.toString() === userId);
-        if (existingApplicant) {
-            throw new ApiError(400, 'User has already applied for this job');
-        }
+//         // Check if the user has already applied for this job
+//         const existingApplicant = job.applicants.find(applicant => applicant.user.toString() === userId);
+//         if (existingApplicant) {
+//             throw new ApiError(400, 'User has already applied for this job');
+//         }
 
-        // Add the user to the job's applicants list
-        job.applicants.push({ user: userId });
+//         // Add the user to the job's applicants list
+//         job.applicants.push({ user: userId });
+//          User.findByIdAndUpdate(userId, { $push: { applied_jobs: jobId } });
 
-        // Save the updated job object
-        await job.save();
 
-        // Return success response
-        return res.status(200).json(new ApiResponse(200, null, 'Application submitted successfully'));
-    // } catch (error) {
-    //     // If an error occurs, handle it and return an error response
-    //     throw new ApiError(500, 'Internal server error');
-    // }
+//         // Save the updated job object
+//         await job.save();
+
+//         // Return success response
+//         return res.status(200).json(new ApiResponse(200, null, 'Application submitted successfully'));
+//     // } catch (error) {
+//     //     // If an error occurs, handle it and return an error response
+//     //     throw new ApiError(500, 'Internal server error');
+//     // }
+// });
+const applyForJob = asyncHandler(async (req, res) => {
+    // Get the job ID from the request parameters
+    const { jobId } = req.params;
+
+    // Get the user ID from the request user object
+    const userId = req.user._id;
+
+    // Find the job by ID
+    const job = await Job.findById(jobId);
+    
+    // Check if the job exists
+    if (!job) {
+        throw new ApiError(404, 'Job not found');
+    }
+
+    // Check if the user has already applied for this job
+    const existingApplicant = job.applicants.find(applicant => applicant.user.toString() === userId);
+    if (existingApplicant) {
+        throw new ApiError(400, 'User has already applied for this job');
+    }
+
+    // Add the user to the job's applicants list
+    job.applicants.push({ user: userId });
+    
+    // Save the updated job object
+    await job.save();
+
+    // Update the applied_jobs array in the user model
+    const user = await User.findById(userId);
+    if (!user) {
+        throw new ApiError(404, 'User not found');
+    }
+    user.applied_jobs.push(jobId);
+    await user.save();
+
+    // Return success response
+    return res.status(200).json(new ApiResponse(200, null, 'Application submitted successfully'));
 });
+
 
 const createJob = asyncHandler(async (req, res) => {
     const {title , description} = req.body
