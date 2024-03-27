c7c15ffd28bcbf561be2263975949fe028c7b75a
mayankk16 <raitanimayank1903@gmail.com>
2024-03-27 17:50:11 +0530

    Updated Post Model

--- src/models/Post.model.js
+++ src/models/Post.model.js
@@ -1,9 +1,5 @@
 import mongoose, {Schema} from "mongoose";
 const postSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
     content: {
         type: String,
         required: true
@@ -11,18 +7,6 @@ const postSchema = new Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Company'
-    },
-    likes: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    shares: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    views: {
-        type: Number,
-        default: 0
     },
     comments: [{
         type: mongoose.Schema.Types.ObjectId,
